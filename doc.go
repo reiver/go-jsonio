80a3ddc4f77c08098741a9eead14908e3c7e5f7c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2021-03-18 21:26:01 -0700

    initial commits

--- doc.go
+++ doc.go
@@ -1,4 +1,6 @@
 /*
 Package jsonio provides tools for working with JSON I/O. This supplements Go's built-in "encoding/json" package.
+
+Package jsonio provides interface's compatible with Gorilla's websocket package's ReadJSON() and WriteJSON() methods.
 */
 package jsonio
